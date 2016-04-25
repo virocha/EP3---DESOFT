fac67dd69142879bdb8269941927cc096c8ab53a
IsabellaOInsper <isabellaro@al.insper.edu.br>
2016-04-25 19:02:52 -0300

    Função verifica ganhador
    Adicionei as combinações possíveis pros jogadores ganharem

--- Jogo.py
+++ Jogo.py
@@ -3,24 +3,44 @@ import numpy as np
 
 
 R = 9 #Resultado
-class MockJogoDaVelha:
+class Jogo:
     def __init__(self):
         self.jogador = 1
         self.matriz = np.zeros([3,3])
         self.rodada = 1
         
     def recebe_jogada(self, linha, coluna): #Recebe e registra uma jogada e alterna jogadores
+        linha, coluna = ***RECEBE VALORES DA FUNÇÃO CLICA***  
         if self.jogador == 1:
             self.jogador = 2
-            self.rodada += 1
             
         else:
             self.jogador = 1
+            
+        self.rodada += 1
     
-    def verifica_ganhador(self): #Verifica se o jogo acabou, retorna 0 em caso de empate, 1 se X ganhou e 2 se Y ganhou, -1 caso contrário
-        if # X ganhou
+    def verifica_ganhador(self): #Verifica se o jogo acabou, retorna 0 em caso de empate, 1 se X ganhou e 2 se O ganhou, -1 caso contrário
+        if  self.matriz[0][0] = self.matriz[0][1] = self.matriz[0][2] = 1 or
+        self.matriz[1][0] = self.matriz[1][1] = self.matriz[1][2] = 1 or
+        self.matriz[2][0] = self.matriz[2][1] = self.matriz[2][2] = 1 or 
+            
+        self.matriz[0][0] = self.matriz[1][0] = self.matriz[2][0] = 1 or
+        self.matriz[0][1] = self.matriz[1][1] = self.matriz[2][1] = 1 or
+        self.matriz[0][2] = self.matriz[1][2] = self.matriz[2][2] = 1 or
+
+        self.matriz[0][0] = self.matriz[1][1] = self.matriz[2][2] = 1 or
+        self.matriz[0][2] = self.matriz[1][1] = self.matriz[2][0] = 1: # X ganhou
             R = 1
-        elif # Y ganhoy
+        elif if self.matriz[0][0] = self.matriz[0][1] = self.matriz[0][2] = 2 or
+        self.matriz[1][0] = self.matriz[1][1] = self.matriz[1][2] = 2 or
+        self.matriz[2][0] = self.matriz[2][1] = self.matriz[2][2] = 2 or 
+                
+        self.matriz[0][0] = self.matriz[1][0] = self.matriz[2][0] = 2 or
+        self.matriz[0][1] = self.matriz[1][1] = self.matriz[2][1] = 2 or
+        self.matriz[0][2] = self.matriz[1][2] = self.matriz[2][2] = 2 or
+                
+        self.matriz[0][0] = self.matriz[1][1] = self.matriz[2][2] = 2 or
+        self.matriz[0][2] = self.matriz[1][1] = self.matriz[2][0] = 2: # O ganhou
             R = 2
         elif self.rodada = 9 #Empate/Velha
             R = 0
