fd48fd70ce625addffd6517095e0904e86dfe40b
IsabellaOInsper <isabellaro@al.insper.edu.br>
2016-04-25 17:32:49 -0300

    Algumas alterações
    Fiz mais algumas alterações mas está bem básico

--- Jogo.py
+++ Jogo.py
@@ -1,22 +1,33 @@
 import tkinter as tk
 import numpy as np
 
-Matriz = np.zeros([3,3])
 
-print(Matriz)
-
-class Jogo:
+R = 9 #Resultado
+class MockJogoDaVelha:
     def __init__(self):
         self.jogador = 1
+        self.matriz = np.zeros([3,3])
+        self.rodada = 1
         
     def recebe_jogada(self, linha, coluna): #Recebe e registra uma jogada e alterna jogadores
         if self.jogador == 1:
             self.jogador = 2
+            self.rodada += 1
             
         else:
             self.jogador = 1
     
     def verifica_ganhador(self): #Verifica se o jogo acabou, retorna 0 em caso de empate, 1 se X ganhou e 2 se Y ganhou, -1 caso contrário
-        return -1
+        if # X ganhou
+            R = 1
+        elif # Y ganhoy
+            R = 2
+        elif self.rodada = 9 #Empate/Velha
+            R = 0
+        else:
+            R = -1
+        return R
         
-    def limpa_jogadas(): #
+    def limpa_jogadas(): #Reinicia o jogo
+    
+    
