db0309b4096e926bb7d4b4546165319174525102
IsabellaOInsper <isabellaro@al.insper.edu.br>
2016-04-25 16:09:35 -0300

    Começando o jogo
    Iniciei as funções do jogo

--- Jogo.py
+++ Jogo.py
@@ -1,11 +1,22 @@
 import tkinter as tk
+import numpy as np
 
-class MockJogoDaVelha:
+Matriz = np.zeros([3,3])
+
+print(Matriz)
+
+class Jogo:
     def __init__(self):
         self.jogador = 1
         
-    def recebe_jogada(self, linha, coluna):
-        pass
+    def recebe_jogada(self, linha, coluna): #Recebe e registra uma jogada e alterna jogadores
+        if self.jogador == 1:
+            self.jogador = 2
+            
+        else:
+            self.jogador = 1
     
-    def verifica_ganhador(self):
+    def verifica_ganhador(self): #Verifica se o jogo acabou, retorna 0 em caso de empate, 1 se X ganhou e 2 se Y ganhou, -1 caso contrário
         return -1
+        
+    def limpa_jogadas(): #
