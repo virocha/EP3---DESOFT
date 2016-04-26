9e0dc7cee04dd92144433873200e2a80d635f532
virocha <vitoriaaar@al.insper.edu.br>
2016-04-26 10:48:49 -0300

    Tabuleiro com todas as funções, mas não recomeça ainda

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,54 +1,89 @@
-import tkinter as tk
+# -*- coding: utf-8 -*-
+"""
+Insper - Engenharia 1B
+Design de Software
+EP3: Jogo da Velha
 
-class MockJogoDaVelha:
-    def __init__(self):
-        self.jogador = 1
-        
-    def recebe_jogada(self, linha, coluna):
-        pass
-    
-    def verifica_ganhador(self):
-        return -1
+Vitória Rocha e Isabella Oliveira
+"""
+
+import tkinter as tk
+import tkinter.messagebox as tkm
+from Jogo import Jogo
 
 class Tabuleiro:
     def __init__(self):
-        
-        self.jogo = MockJogoDaVelha()
-        
+        self.meu_jogo = Jogo() 
+
+        # Janela principal
         self.window = tk.Tk()
-        self.window.geometry("300x300+100+100")
-        self.window.rowconfigure(0, minsize = 100)
-        self.window.rowconfigure(1, minsize = 100)
-        self.window.rowconfigure(2, minsize = 100)
-        self.window.rowconfigure(3, minsize = 50)
-        self.window.columnconfigure(0, minsize = 100)
-        self.window.columnconfigure(1, minsize = 100)
-        self.window.columnconfigure(2, minsize = 100)
+        self.window.title("Jogo da Velha")
+        self.window.geometry("450x520")
         
-        self.chama_jogador = tk.Label(self.window)
-        self.chama_jogador.configure(text = "Jogador 1: X")
-        self.chama_jogador.grid(row = 3, column = 0, columnspan = 3, sticky = "nsew")
-               
-        self.botao1 = tk.Button(self.window)
-        self.botao1.grid(row = 1, column = 0, sticky  = "nsew")
-        self.botao2 = tk.Button(self.window)
-        self.botao2.grid(row = 1, column = 1, sticky  = "nsew")
-        self.botao3 = tk.Button(self.window)
-        self.botao3.grid(row = 1, column = 2, sticky  = "nsew")
-        self.botao4 = tk.Button(self.window)
-        self.botao4.grid(row = 2, column = 0, sticky  = "nsew")
-        self.botao5 = tk.Button(self.window)
-        self.botao5.grid(row = 2, column = 1, sticky  = "nsew")
-        self.botao6 = tk.Button(self.window)
-        self.botao6.grid(row = 2, column = 2, sticky  = "nsew")
-        self.botao7 = tk.Button(self.window)
-        self.botao7.grid(row = 3, column = 0, sticky  = "nsew")
-        self.botao8 = tk.Button(self.window)
-        self.botao8.grid(row = 3, column = 1, sticky  = "nsew")
-        self.botao9 = tk.Button(self.window)
-        self.botao9.grid(row = 3, column = 2, sticky  = "nsew")
+        for i in range(0, 3):
+            self.window.rowconfigure(i, minsize=150)
+            self.window.columnconfigure(i, minsize=150)
         
+        self.window.rowconfigure(3, minsize=40)
+
+        # Botões
+        global botoes 
+        botoes = [[[0],[0],[0]],[[0],[0],[0]],[[0],[0],[0]]] 
         
+        for i in range(0, 3): 
+            for j in range(0, 3): 
+                botoes[i][j] = tk.Button(self.window) 
+                botoes[i][j].configure(height=10, width=10, command=lambda botao=(i,j): self.botao_comando(botao))
+                botoes[i][j].grid(row=i, column=j, sticky="nsew")
+        
+        #  Labels
+        global label_1 
+        
+        label_1 = tk.Label(self.window) 
+        label_1.configure(width=60, height=1)
+        label_1.grid(row=3, column=0, columnspan=3, sticky="w")
+        
+        self.cria_label("Primeira Jogada: {0}".format(self.meu_jogo.jogada))
+    
+    def cria_botoes(self, posicao, limpar_tabuleiro):
+        texto_botao = tk.StringVar()
+        
+        if limpar_tabuleiro == False:
+            texto_botao.set(self.meu_jogo.jogada) 
+        else:
+            texto_botao.set("") 
+
+        global botoes
+        botoes[posicao[0]][posicao[1]].config(textvariable = texto_botao,\
+                state="disabled") 
+        
+        self.cria_label("Próxima Jogada: {0}".format(self.meu_jogo.proxima_jogada))
+
+    def cria_label(self, display): 
+        texto_label = tk.StringVar()
+        texto_label.set(display)
+        
+        global label_1
+        label_1.configure(textvariable = texto_label, anchor="w")
+    
+    def botao_comando(self, posicao): 
+        
+        self.cria_botoes(posicao, False)
+        self.meu_jogo.recebe_jogada(posicao)
+
+        resultado = self.meu_jogo.verifica_ganhador()
+
+    def limpa_tela(self): 
+        for i in range(0,3):
+            for j in range(0,3):
+                self.cria_botoes((i,j), True) 
+                botoes[i][j].config(state="normal") 
+        
+        self.cria_label("Primeira Jogada: {0}".format(self.meu_jogo.jogada)) 
+
+    def iniciar(self):
+        self.window.mainloop()
+
        
 app = Tabuleiro()
 app.iniciar()
