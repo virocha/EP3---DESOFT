7bdabb342f8c4a6720cfb969095d1b8444913f17
IsabellaOInsper <isabellaro@al.insper.edu.br>
2016-04-26 11:51:37 -0300

    Jogo terminado, graças a Deus!!! <3 Vivaaaaa!

--- Jogo.py
+++ Jogo.py
@@ -1,16 +1,11 @@
-import tkinter as tk
 import numpy as np
 
-
-R = 9 #Resultado
 class Jogo:
     def __init__(self):
-        self.jogador = 1
-        self.matriz = np.zeros([3,3])
-        self.rodada = 1
+        self.limpa_jogadas()
         
     def recebe_jogada(self, linha, coluna): #Recebe e registra uma jogada e alterna jogadores
-        #linha, coluna = #***RECEBE VALORES DA FUNÇÃO CLICA***  
+        self.matriz[linha][coluna] = self.jogador
         if self.jogador == 1:
             self.jogador = 2
             
@@ -20,9 +15,23 @@ class Jogo:
         self.rodada += 1
     
     def verifica_ganhador(self): #Verifica se o jogo acabou, retorna 0 em caso de empate, 1 se X ganhou e 2 se O ganhou, -1 caso contrário
-        if  self.matriz[0][0] == self.matriz[0][1] == self.matriz[0][2] == 1 or self.matriz[1][0] == self.matriz[1][1] == self.matriz[1][2] == 1 or self.matriz[2][0] == self.matriz[2][1] == self.matriz[2][2] == 1 or self.matriz[0][0] == self.matriz[1][0] == self.matriz[2][0] == 1 or self.matriz[0][1] == self.matriz[1][1] == self.matriz[2][1] == 1 or self.matriz[0][2] == self.matriz[1][2] == self.matriz[2][2] == 1 or self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] == 1 or self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] == 1: # X ganhou
+        if  self.matriz[0][0] == self.matriz[0][1] == self.matriz[0][2] == 1 or \
+            self.matriz[1][0] == self.matriz[1][1] == self.matriz[1][2] == 1 or \
+            self.matriz[2][0] == self.matriz[2][1] == self.matriz[2][2] == 1 or \
+            self.matriz[0][0] == self.matriz[1][0] == self.matriz[2][0] == 1 or \
+            self.matriz[0][1] == self.matriz[1][1] == self.matriz[2][1] == 1 or \
+            self.matriz[0][2] == self.matriz[1][2] == self.matriz[2][2] == 1 or \
+            self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] == 1 or \
+            self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] == 1: # X ganhou
             R = 1
-        elif self.matriz[0][0] == self.matriz[0][1] == self.matriz[0][2] == 2 or self.matriz[1][0] == self.matriz[1][1] == self.matriz[1][2] == 2 or self.matriz[2][0] == self.matriz[2][1] == self.matriz[2][2] == 2 or self.matriz[0][0] == self.matriz[1][0] == self.matriz[2][0] == 2 or self.matriz[0][1] == self.matriz[1][1] == self.matriz[2][1] == 2 or self.matriz[0][2] == self.matriz[1][2] == self.matriz[2][2] == 2 or self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] == 2 or self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] == 2: # O ganhou
+        elif self.matriz[0][0] == self.matriz[0][1] == self.matriz[0][2] == 2 or \
+             self.matriz[1][0] == self.matriz[1][1] == self.matriz[1][2] == 2 or \
+             self.matriz[2][0] == self.matriz[2][1] == self.matriz[2][2] == 2 or \
+             self.matriz[0][0] == self.matriz[1][0] == self.matriz[2][0] == 2 or \
+             self.matriz[0][1] == self.matriz[1][1] == self.matriz[2][1] == 2 or \
+             self.matriz[0][2] == self.matriz[1][2] == self.matriz[2][2] == 2 or \
+             self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] == 2 or \
+             self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] == 2: # O ganhou
             R = 2
         elif self.rodada == 9: #Empate/Velha
             R = 0
@@ -31,5 +40,12 @@ class Jogo:
         return R
         
     def limpa_jogadas(self): #Reinicia o jogo
-        return self.matriz == np.zeros([3,3])
+        self.jogador = 1
+        self.matriz = np.zeros([3,3])
+        self.rodada = 1        
+        
+if __name__ == "__main__":
+    jogo = Jogo()
+    
+
 
