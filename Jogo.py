afb40e55fb1f931498a866378b8db43a9546e658
virocha <vitoriaaar@al.insper.edu.br>
2016-04-27 07:19:37 -0300

    Final!!!!

--- Jogo.py
+++ Jogo.py
@@ -1,19 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Insper - Engenharia 1B
+Design de Software
+EP3: Jogo da Velha
+
+Vitória Rocha e Isabella Oliveira
+"""
+
 import numpy as np
 
 class Jogo:
     def __init__(self):
-        self.limpa_jogadas()
+        self.jogador = "X"
+        self.proximo = "O"
+        self.matriz = np.zeros([3,3])
+        self.resultado = 0      
         
-    def recebe_jogada(self, linha, coluna): #Recebe e registra uma jogada e alterna jogadores
-        self.matriz[linha][coluna] = self.jogador
-        if self.jogador == 1:
-            self.jogador = 2
+    def recebe_jogada(self, posicao): 
+        self.guarda_jogada(posicao)
+        
+        if self.jogador == "X":
+            self.proximo = "O"
             
-        else:
-            self.jogador = 1
+        elif self.jogador == "O":
+            self.proximo = "X"
             
-        self.rodada += 1
-    
+        self.resultado += 1
+            
+    def guarda_jogada(self, posicao):
+        if self.jogador == "X":
+            identificacao = 1
+        
+        elif self.jogador == "O":
+            identificacao = 2
+        
+        self.matriz[posicao[0]][posicao[1]] = identificacao
+        
+        self.jogador = self.proximo
+        
     def verifica_ganhador(self): #Verifica se o jogo acabou, retorna 0 em caso de empate, 1 se X ganhou e 2 se O ganhou, -1 caso contrário
         if  self.matriz[0][0] == self.matriz[0][1] == self.matriz[0][2] == 1 or \
             self.matriz[1][0] == self.matriz[1][1] == self.matriz[1][2] == 1 or \
@@ -33,16 +57,30 @@ class Jogo:
              self.matriz[0][0] == self.matriz[1][1] == self.matriz[2][2] == 2 or \
              self.matriz[0][2] == self.matriz[1][1] == self.matriz[2][0] == 2: # O ganhou
             R = 2
-        elif self.rodada == 9: #Empate/Velha
+        elif self.resultado == 9: #Empate/Velha
             R = 0
         else:
             R = -1
         return R
         
-    def limpa_jogadas(self): #Reinicia o jogo
-        self.jogador = 1
+    def limpa_jogadas(self): #Reinicia o jogo e define o jogador que vai iniciar o jogo
+        
+        if self.resultado == "X": 
+            self.jogador = "X"
+            self.proximo = "O"
+        
+        elif self.resultado == "O":
+            self.jogador = "O"
+            self.proximo = "X"
+
+        elif self.resultado == -1: 
+            self.jogador = self.proximo
+            if self.jogador == "X":
+                self.proximo = "O"
+            else:
+                self.proximo = "X"
+        
         self.matriz = np.zeros([3,3])
-        self.rodada = 1        
         
 if __name__ == "__main__":
     jogo = Jogo()
--- Tabuleiro.py
+++ Tabuleiro.py
@@ -27,44 +27,37 @@ class Tabuleiro:
         self.window.rowconfigure(3, minsize=40)
 
         # Botões
-        global botoes 
-        botoes = [[[0],[0],[0]],[[0],[0],[0]],[[0],[0],[0]]] 
+        self.botoes = [[[0],[0],[0]],[[0],[0],[0]],[[0],[0],[0]]] 
         
         for i in range(0, 3): 
             for j in range(0, 3): 
-                botoes[i][j] = tk.Button(self.window) 
-                botoes[i][j].configure(height=10, width=10, command=lambda botao=(i,j): self.botao_comando(botao))
-                botoes[i][j].grid(row=i, column=j, sticky="nsew")
+                self.botoes[i][j] = tk.Button(self.window) 
+                self.botoes[i][j].configure(height=10, width=10, command=lambda botao=(i,j): self.botao_comando(botao))
+                self.botoes[i][j].grid(row=i, column=j, sticky="nsew")
+                                              
+        # Labels
+        self.label_indicador = tk.Label(self.window) 
+        self.label_indicador.configure(width=60, height=1)
+        self.label_indicador.grid(row=3, column=0, columnspan=3, sticky="w")
         
-        #  Labels
-        global label_1 
-        
-        label_1 = tk.Label(self.window) 
-        label_1.configure(width=60, height=1)
-        label_1.grid(row=3, column=0, columnspan=3, sticky="w")
-        
-        self.cria_label("Primeira Jogada: {0}".format(self.meu_jogo.jogada))
-    
+        self.cria_label("Primeira jogada: {0}".format(self.meu_jogo.jogador))
+                    
     def cria_botoes(self, posicao, limpar_tabuleiro):
         texto_botao = tk.StringVar()
-        
+        texto_botao.set("")
+       
         if limpar_tabuleiro == False:
-            texto_botao.set(self.meu_jogo.jogada) 
-        else:
-            texto_botao.set("") 
-
-        global botoes
-        botoes[posicao[0]][posicao[1]].config(textvariable = texto_botao,\
+            texto_botao.set(self.meu_jogo.jogador)
+        
+        self.botoes[posicao[0]][posicao[1]].config(textvariable = texto_botao,\
                 state="disabled") 
         
-        self.cria_label("Próxima Jogada: {0}".format(self.meu_jogo.proxima_jogada))
+        self.cria_label("Próxima Jogada: {0}".format(self.meu_jogo.proximo))
 
-    def cria_label(self, display): 
-        texto_label = tk.StringVar()
-        texto_label.set(display)
-        
-        global label_1
-        label_1.configure(textvariable = texto_label, anchor="w")
+    def cria_label(self, texto): 
+        label_indicador = tk.StringVar()
+        label_indicador.set(texto)
+        self.label_indicador.configure(textvariable = label_indicador, anchor="w")
     
     def botao_comando(self, posicao): 
         
@@ -72,18 +65,46 @@ class Tabuleiro:
         self.meu_jogo.recebe_jogada(posicao)
 
         resultado = self.meu_jogo.verifica_ganhador()
+        
+        if resultado == -1: 
+            pass
+        elif resultado == 0:
+            self.quem_venceu("O jogo empatou!")
+        elif resultado == 1:
+            self.quem_venceu("Vencedor: Jogador X")
+        elif resultado == 2:
+            self.quem_venceu("Vencedor: Jogador O")
+            
+    def quem_venceu(self, resultado): 
+        
+        self.nova_janela = tkm.askquestion("Resultado", "{0}\nDeseja começar um novo jogo?".format(resultado))        
+        
+        self.meu_jogo.limpa_jogadas()
+        self.limpa_tela()
+
+        if self.nova_janela == "no":
+            self.window.destroy()
 
     def limpa_tela(self): 
+        
         for i in range(0,3):
             for j in range(0,3):
                 self.cria_botoes((i,j), True) 
-                botoes[i][j].config(state="normal") 
+                self.botoes[i][j].config(state="normal") 
         
-        self.cria_label("Primeira Jogada: {0}".format(self.meu_jogo.jogada)) 
+        self.cria_label("Primeira Jogada: {0}".format(self.meu_jogo.jogador)) 
+        
 
     def iniciar(self):
         self.window.mainloop()
-
+        
+        
+class Final_Jogo:
+    def __init__(self):
+        self.Final_Jogo = tk.Tk()
+        self.Final_Jogo.title("Resultado do Jogo da Velha")
+        self.Final_Jogo.geometry("100x200")
+       
        
 app = Tabuleiro()
 app.iniciar()
